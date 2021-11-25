8d69766de805b18c8d898ce937a0ef11a9d7e930
Banalian <64379456+Banalian@users.noreply.github.com>
2021-11-25 15:00:41 +0100

    feat(tags): created YearTag class

---
+++ src/main/java/fr/ppptours/cafetaria/model/tags/YearTag.java
@@ -0,0 +1,15 @@
+package fr.ppptours.cafetaria.model.tags;
+
+/**
+ * Tag Class to represent a year/class ( for example a client being in 3rd year, or in PEIP)
+ */
+public class YearTag extends Tag {
+
+    /**
+     * Constructor
+     * @param tagName name for the created tag
+     */
+    public YearTag(String tagName) {
+        super(tagName);
+    }
+}
