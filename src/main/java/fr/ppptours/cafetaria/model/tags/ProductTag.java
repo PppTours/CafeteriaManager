ca80197f6f38d424ee4baf1175143f5d5342f69b
Banalian <64379456+Banalian@users.noreply.github.com>
2021-11-25 15:00:30 +0100

    feat(tags): created ProductTag class

---
+++ src/main/java/fr/ppptours/cafetaria/model/tags/ProductTag.java
@@ -0,0 +1,16 @@
+package fr.ppptours.cafetaria.model.tags;
+
+/**
+ * Tag class for products.
+ */
+public class ProductTag extends Tag {
+
+    /**
+     * Constructor
+     * @param tagName name for the created tag
+     */
+    public ProductTag(String tagName) {
+        super(tagName);
+    }
+
+}
